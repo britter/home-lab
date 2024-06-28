4c02dbadd10eeb573a1fc820a6c640b605a211af
Benedikt Ritter <beneritter@gmail.com>
2024-06-28 10:33:09 +0200

    cloudlfare: Add DNS record for nextcloud

--- cloudflare/cyberoffice.tf
+++ cloudflare/cyberoffice.tf
@@ -1,7 +1,19 @@
+locals {
+  cyberoffice_ip = "192.168.178.200"
+}
+
 resource "cloudflare_record" "cyberoffice_host" {
   zone_id = var.cloudflare_zone_id
   name    = "cyberoffice"
-  value   = "192.168.178.200"
+  value   = local.cyberoffice_ip
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "nextcloud" {
+  zone_id = var.cloudflare_zone_id
+  name    = "nextcloud"
+  value   = local.cyberoffice_ip
   type    = "A"
   proxied = false
 }
