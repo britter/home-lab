077d5aef6c7c3ee48833e9cdc6b974de414cc0c7
Benedikt Ritter <beneritter@gmail.com>
2024-06-28 14:01:06 +0200

    cloudflare: Create DNS records for Adguard and FirtzBox

---
+++ cloudflare/directions.tf
@@ -0,0 +1,27 @@
+locals {
+  directions_ip = "192.168.178.105"
+}
+
+resource "cloudflare_record" "directions_host" {
+  zone_id = var.cloudflare_zone_id
+  name    = "directions"
+  value   = local.watchtower_ip
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "adguard" {
+  zone_id = var.cloudflare_zone_id
+  name    = "adguard"
+  value   = local.directions_ip
+  type    = "A"
+  proxied = false
+}
+
+resource "cloudflare_record" "fritz_box" {
+  zone_id = var.cloudflare_zone_id
+  name    = "fritz-box"
+  value   = local.directions_ip
+  type    = "A"
+  proxied = false
+}
