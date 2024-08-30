96030ec63ab4265fadee55832e8950eff92b721e
Benedikt Ritter <beneritter@gmail.com>
2024-08-30 19:49:15 +0200

    cloudflare: Manage API tokens for prod servers via terraform

--- cloudflare/srv-prod-1.tf
+++ cloudflare/srv-prod-1.tf
@@ -9,3 +9,21 @@ resource "cloudflare_record" "srv_prod_1_wildcard" {
   type    = "A"
   proxied = false
 }
+
+resource "cloudflare_api_token" "srv_prod_1_acme_token" {
+  name = "srv-prod-1 acme token"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.zone.${var.cloudflare_zone_id}" = "*"
+    }
+  }
+}
+
+output "srv_prod_1_acme_token_value" {
+  value     = cloudflare_api_token.srv_prod_1_acme_token.value
+  sensitive = true
+}
--- cloudflare/srv-prod-2.tf
+++ cloudflare/srv-prod-2.tf
@@ -9,3 +9,21 @@ resource "cloudflare_record" "srv_prod_2_wildcard" {
   type    = "A"
   proxied = false
 }
+
+resource "cloudflare_api_token" "srv_prod_2_acme_token" {
+  name = "srv-prod-2 acme token"
+
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.zone.${var.cloudflare_zone_id}" = "*"
+    }
+  }
+}
+
+output "srv_prod_2_acme_token_value" {
+  value     = cloudflare_api_token.srv_prod_2_acme_token.value
+  sensitive = true
+}
