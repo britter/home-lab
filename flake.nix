49e19ee957831eddbf99b566bb228bd6d90f5cc3
Benedikt Ritter <beneritter@gmail.com>
2024-10-26 11:56:25 +0200

    Manage terraform via nix devShell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          terraform
+          terraform-ls
+        ];
+        shellHook = ''
+          fish
+        '';
+      };
+    }
+  );
+}
